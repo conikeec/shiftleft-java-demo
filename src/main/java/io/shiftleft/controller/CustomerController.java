aa7997073b7cec785b220235601dc5f612d9c826
Chetan Conikee <conikee@gmail.com>
2024-08-08 11:17:59 -0700

    Fixing src/main/java/io/shiftleft/controller/CustomerController.java for finding 1041

--- src/main/java/io/shiftleft/controller/CustomerController.java
+++ src/main/java/io/shiftleft/controller/CustomerController.java
@@ -182,6 +182,30 @@ public class CustomerController {
       return customer;
     }
 
+	public Customer getCustomer(@PathVariable("customerId") Long customerId) {
+
+		/* validate customer Id parameter */
+      if (null == customerId) {
+        throw new InvalidCustomerRequestException();
+      }
+
+      Customer customer = customerRepository.findOne(customerId);
+		if (null == customer) {
+		  throw new CustomerNotFoundException();
+	  }
+
+	  // Removed logging of sensitive account data
+
+      try {
+        dispatchEventToSalesForce(String.format(" Customer %s Logged into SalesForce", customer));
+      } catch (Exception e) {
+        log.error("Failed to Dispatch Event to SalesForce . Details {} ", e.getLocalizedMessage());
+
+      }
+
+      return customer;
+    }
+
 	@RequestMapping(value = "/customers/{customerId}", method = RequestMethod.GET)
 	public Customer getCustomer(@PathVariable("customerId") Long customerId) {
 
@@ -479,6 +503,7 @@ public class CustomerController {
 	}
 
 }
+
 
 
 

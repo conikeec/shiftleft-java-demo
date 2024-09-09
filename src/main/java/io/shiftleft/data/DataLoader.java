40b64251eaaeab2c4161393e8ccd508c243bd0ed
conikeec <conikee@gmail.com>
2024-09-08 17:35:18 -0700

    deep learning based cognition

--- src/main/java/io/shiftleft/data/DataLoader.java
+++ src/main/java/io/shiftleft/data/DataLoader.java
@@ -56,13 +56,13 @@ public class DataLoader implements CommandLineRunner {
   }
 
   public final class Constants {
-    
-        private Constants() {
-                // restrict instantiation
-        }
-    
-        public static final double PI = 3.14159;
-        public static final double PLANCK_CONSTANT = 6.62606896e-34;
+
+    private Constants() {
+      // restrict instantiation
+    }
+
+    public static final double PI = 3.14159;
+    public static final double PLANCK_CONSTANT = 6.62606896e-34;
   }
 
   private boolean connectToAws() {
@@ -102,7 +102,6 @@ public class DataLoader implements CommandLineRunner {
 
     connectToAws();
 
-    log.debug("Loading test data...");
     for (Customer customer : dataBuilder.createCustomers()) {
       customerRepository.save(customer);
     }
